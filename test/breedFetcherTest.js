c912f2fa2eef9e6387c3061828f4ec4c3d5f38dc
Clifford Tse <TseClifford@gmail.com>
2022-01-20 21:27:39 +0000

    updated test descriptions

--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -4,12 +4,10 @@ const { assert } = require('chai');
 describe('fetchBreedDescription', () => {
   it('returns a string description for a valid breed, via callback', (done) => {
     fetchBreedDescription('Siberian', (err, desc) => {
-      // we expect no error for this scenario
       assert.equal(err, null);
 
       const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
 
-      // compare returned description
       assert.equal(expectedDesc, desc.trim());
 
       done();
@@ -18,12 +16,10 @@ describe('fetchBreedDescription', () => {
 
   it('returns a null description for a invalid breed', (done) => {
     fetchBreedDescription('Siber123', (err, desc) => {
-      // we expect no error for this scenario
       assert.equal(err, `The requested breed is not found.`);
 
       const expectedDesc = null;
 
-      // compare returned description
       assert.equal(expectedDesc, desc);
 
       done();
